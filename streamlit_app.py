5f44ff33b300c8f7466732467031763d62a1d70e
Misriya-23 <125578943+Misriya-23@users.noreply.github.com>
2023-02-16 20:51:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit
+import pandas
+
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favourite')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boilde Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
-
-  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+

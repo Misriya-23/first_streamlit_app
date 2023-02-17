2387959a8e0dab951b361a7ec9c3e7da6dd8b188
Misriya-23 <125578943+Misriya-23@users.noreply.github.com>
2023-02-17 21:42:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+
+from urllib.error import URLError
